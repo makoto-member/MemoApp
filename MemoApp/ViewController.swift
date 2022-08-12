7658858135dfae31d842a0150d7bc81af1db5b72
makoto <kuleima.makoto@gmail.com>
2022-08-13 08:14:53 +0900

    Initial Commit

---
+++ MemoApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 橋詰真 on 2022/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+        
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        textView.text = sceneDelegate.lastText
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        sceneDelegate.lastText = textView.text
+    }
+
+
+}
+
